d07d3bf3de99462bf4f4d8df59ae140886f5f897
Franverri <francoetche@gmail.com>
2021-06-15 13:52:02 -0300

    Initial files

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# CP-M1-Yoda
+# M1 - Checkpoint
+
+### Instrucciones
+
+Te vamos a brindar los tests preparados, tu tarea es completar el código en `checkpoint.js` de tal forma que pasen la mayoría de los tests. Para ejecutar los tests, primero debes ejecutar el comando `npm install` dentro de esta carpeta (`CP-M1`). Luego, en esta misma carpeta ejecutar `npm test`.
+
+### Subir tu checkpoint
+
+Una vez terminado el checkpoint, subir los cambios a tu repositorio.
+
+**Notas**:
+
+* Sólo podés consultar a los PM y al instructor por problemas de instrucciones o si no te funcionan los tests.
+* Hay cosas que tal vez no vimos específicamente en los lectures, vas a tener que investigar un poquito por tu cuenta.
+* Tenes que subir el resultado del challenge antes de las 18:00 PM horario Argentina.
+* Podés buscar en internet o en cualquier otro lugar por recursos para que te ayudes.
+* No podés ayudar ni preguntar a tus compañeros de clases.
+
+### Condiciones de aprobación
+
+Para aprobar el checkpoint es necesario contar con un mínimo de 17 tests pasando correctamente sobre los 25 totales.
---
+++ checkpoint.js
@@ -0,0 +1,292 @@
+// ----- IMPORTANTE -----
+
+// IMPORTANTE!: Para este checkpoint tendrán en el archivo DS.js las implementaciones ya realizadas en las
+// homeworks de Queue, LinkedLis y BinarySearchTree. Sobre dicha implementación van a tener que agregar nuevos
+// métodos o construir determinadas funciones explicados más abajo. Pero todos los métodos ya implementados
+// en las homeowrks no es necesario que los vuelvan a definir.
+// NO DEBEN MODIFICAR EL ARCHIVO DS.js SINO QUE TODO SU CÓDIGO TENDRÁ QUE ESTAR EN ESTE ARCHIVO checkpoint.js
+
+const {
+  Queue,
+  Node,
+  LinkedList,
+  BinarySearchTree
+} = require('./DS.js');
+
+// ----------------------
+
+// ----- Recursión -----
+
+// EJERCICIO 1
+// Implementar la función isAncestor: debe determinar si dado dos nombres de personas las mismas
+// son parientes o no (La primera debe ser ancestro de la segunda). La función recibira un objeto
+// que va a representar sólo la parte femenina del "arbol genealogico" familiar y será de la siguiente forma:
+// const genealogyTree = {
+//   "Mona Simpson": [],
+//   "Marge Simpson": ["Lisa Simpson", "Maggie Simpson"],
+//   "Jacqueline Bouvier": ["Marge Simpson", "Patty Bouvier", "Selma Bouvier"],
+//   "Patty Bouvier": [],
+//   "Selma Bouvier": ["Ling Bouvier"],
+//   "Edwina": ["Abigail Simpson"],
+//   "Lisa Simpson": [],
+//   "Maggie Simpson": [],
+//   "Ling Bouvier": []
+// }
+// Ejemplo:
+//  - Caso que devuelve true --> isAncestor(genealogyTree, "Jacqueline Bouvier", "Maggie Simpson")
+//  - Caso que devuelve false --> isAncestor(genealogyTree, "Jacqueline Bouvier", "Abigail Simpson")
+//  [Observar los tests para otros casos]
+
+var isAncestor = function(genealogyTree, ancestor, descendant){
+  // Tu código aca:
+
+}
+
+
+// EJERCICIO 2
+// Secuencia inventada: f(n) = f(n-1) x f(n-2) - f(n-2)
+// Donde las primeras dos posiciones son dadas por el parametro recibidos y a partir de
+// la siguiente se calcula como la multiplicación de los 2 números anteriores restados al número anterior.
+// object es un objeto del cual debemos obtener f(0) y f(1) siguiendo la siguiente lógica:
+// f(0) será un número igual a la cantidad de propiedades del objeto que sean números
+// f(1) será un número igual a la suma de las longitudes de arreglos que sean valores de alguna propiedad
+// Por ejemplo si recibimos: 
+// var obj = {
+//   1: true,
+//   a: [1,2,3],
+//   7: ['F','r','a','n','c','o!'],
+//   h: {a: 1},
+//   z: []
+// }
+// deberíamos tener los siguientes 2 valores iniciales
+// f(0) = 2 y f(1) = 9 (Ya que 1 y 7 son las dos propiedades numéricas y los 3 arreglos que están
+// como valores del objeto suman 9 entradas entre los tres (1,2,3,'F','r','a','n','c','o!').
+// A partir de ahí la tercera posición sería  9 x 2 - 2 = 16 y así sucesivamente
+// La función secuenciaHenry debe devolver el enésimo numero de la serie, por ejemplo para el objeto
+// antes mencionado:
+// secuencia: 2, 9, 16, 135, 2144, 289305
+// secuenciaHenry(0) // 2  ya que el elemento de la posición 0 es cero
+// secuenciaHenry(1) // 9 ya que el elemento de la posición 1 es 1
+// secuenciaHenry(5) // 289305 ya que el elemento de la posición 5 es 289305
+// Para números negativos de n debe devolver false
+function secuenciaHenry(obj, n) {
+  // Tu código aca:
+
+}
+
+// ---------------------
+
+// ----- LinkedList -----
+
+// EJERCICIO 3
+// Implementar el método size dentro del prototype de LinkedList que deberá retornar el tamaño actual de
+// la LinkedList. En el caso de que la lista se encuentre vacía deberá retornar cero.
+// Ejemplo:
+//    var lista = new LinkedList();
+//    lista.size(); --> 0
+//    lista.add(1);
+//    lista.size(); --> 1
+//    lista.add(2);
+//    lista.add(3);
+//    lista.size(); --> 3
+
+LinkedList.prototype.size = function(){
+  // Tu código aca:
+
+}
+
+
+// EJERCICIO 4
+// Implementar el método switchPos dentro del prototype de LinkedList que deberá intercambiar
+// el elemento que se encuentre en pos1 con el elemento en pos2
+// En el caso de que alguna de las dos posiciones no sea válida (Supere el tamaño de la lista actual 
+// o sea un número negativo) debe devolver false.
+// Si los nodos fueron removidos correctamente devolver true.
+// Aclaración: la posición cero corresponde al head de la LinkedList
+// Ejemplo 1:
+//    Suponiendo que la lista actual es: Head --> [1] --> [2] --> [3] --> [4] --> [5]
+//    lista.switchPos(1,3);
+//    Ahora la lista quedaría: Head --> [1] --> [4] --> [3] --> [2] --> [5]
+//    y la función debería haber devuelto true
+// Ejemplo 2:
+//    Suponiendo que se pide una posición inválida: removeFromPos(8) --> false
+// PISTA: Vas a necesitar guardar la referencia a la posición previa y la actual de cada
+// nodo a intercambiar
+
+LinkedList.prototype.switchPos = function(pos1, pos2){
+  // Tu código aca:
+
+}
+
+// EJERCICIO 5
+// Implementar la función mergeLinkedLists que, a partir de dos listas simplemente enlazadas 
+// del mismo tamaño retorne una nueva lista con los elementos de ambas listas
+// Ejemplo:
+//    Lista 1: Head --> 1 --> 7 --> 20 --> null
+//    Lista 2: Head --> 4 --> 13 --> 2 --> null
+//    Lista nueva luego de aplicar mergeLinkedLists:
+//             Head --> 1 --> 4 --> 7 --> 13 --> 20 --> 2 --> null
+
+var mergeLinkedLists = function(linkedListOne, linkedListTwo){
+  // Tu código aca:
+
+}
+
+
+// ----------------------
+
+
+// ----- QUEUE -----
+
+// EJERCICIO 6
+// Implementar la función cardGame: a partir de dos Queues que va a recibir como paráemtro que
+// van a representar mazos de cartas de dos jugadores debemos determinar quien va a ser el ganador
+// de este juego que va a tener la siguiente dinámica:
+// - Los jugadores tendrán que defender su "Castillo" que contiene un total de 100 puntos de resistencia
+// - Cada carta tendrá puntos de ataque (attack), puntos de defensa (defense) y tipo (type)
+// - Ambos jugadores van a sacar las dos primeras cartas de su mazo
+//      * La primera carta será su carta asignada para atacar
+//      * La segunda carta será su carta asignada para defender
+// - La carta asignada para atacar del jugador uno se enfrentará contra la carta asignada para defender
+//   del jugador dos y viceversa. Si el ataque supera los puntos de defensa el daño sobrante será aplicado
+//   sobre el castillo.
+// - El tipo de la carta puede ayudar al ataque o la defensa. Existen 3 tipos ('Destructor', 'Protector', 'Neutral')
+//      * Destructor: si la carta de ataque contiene este tipo se duplicarán sus puntos de ataque
+//      * Protector: si la carta de defensa contiene este tipo se dupicarán sus puntos de defensa
+//      * Neutral: Lamentablemente no aporta nada extra
+// - El juego finaliza cuando alguno de los dos castillos se quede sin puntos de resistencia o cuando los mazos
+//   se acaben. En este último caso ganará aquel jugador que tenga mayor cantidad de puntos de resistencia
+//   restantes en su castillo.
+// La función deberá devolver on objeto que contenga 3 propiedades:
+//  - winner: 'PLAYER ONE' o 'PLAYER TWO' dependiendo quien sea el ganador. En el caso de empate devolver 'TIE'
+//  - castleOne: la cantidad de puntos de resistencia finales del player one
+//  - castleTwo: la cantidad de puntos de resistencia finales del player two
+// NOTA: Ambos mazos contienen la misma cantidad de cartas
+
+
+var cardGame = function(playerOneCards, playerTwoCards){
+  // Tu código aca:
+
+}
+
+// ---------------
+
+
+// ----- BST -----
+
+// EJERCICIO 7
+// Implementar la función height dentro del prototype de BinarySearchTree que debe devolver la "altura"
+// máxima del arbol recibido por parámetro.
+// Ejemplo:
+//             16             ---> Nivel 1
+//          /      \
+//        6         23        ---> Nivel 2
+//      /  \       /   \
+//     2    14    17    31    ---> Nivel 3
+//      \
+//       5                    ---> Nivel 4
+// Este arbol tiene una altura de 4
+// PISTA: Una forma de resolverlo es pensarlo recursivamente y usando Math.max
+
+BinarySearchTree.prototype.height = function(){
+  // Tu código aca:
+
+}
+
+
+// ---------------
+
+
+// Ejercicio 8
+// Dado un arreglo ordenado, encontrar el índice de un elemento específico pasado como parámetro
+// utilizando el método conocido como búsqueda binaria. En el caso de que el número buscado no se encuentre
+// en el array devolver -1.
+// Para mayor información sobre dicho método:
+//    - https://www.khanacademy.org/computing/computer-science/algorithms/binary-search/a/binary-search
+//    - https://en.wikipedia.org/wiki/Binary_search_algorithm
+// Ejemplo:
+//    array = [1,2,3,4,5,6,7,8,9,10];
+//    binarySearch(array, 2) --> Devolvería 1 ya que array[1] = 2
+//    [Donde 2 sería el número sobre el cuál queremos saber su posición en el array]
+
+
+var binarySearch = function (array, target) {
+  // Tu código aca:
+
+}
+
+// EJERCICIO 9
+// Ordená un arreglo de objetos usando un bubble sort pero con algunas particularidades.
+// Además del arreglo a ordenar (array) la función va a recibir como parámetro una función
+// que va a ser quien va a determinar si un elemento es "mayor" al otro para determinar su
+// posición final
+// Ejemplo:
+// var array = [
+//   {name: 'Franco', age: 26, height: 1.85},
+//   {name: 'Toni', age: 30, height: 1.75},
+//   {name: 'Mati', age: 25, height: 1.77},
+//   {name: 'Leo', age: 40, height: 1.83}
+// ]
+//
+// orderFunction(array[0], array[1]) --> Devolvera 1 si están bien ordenados o -1 si hay que intercambiarlos
+// Suponiendo que la orderFunction devuelve -1 si la edad del segundo elemento es menor que la del primero
+// specialSort(array, orderFunction) --> Retornaría el siguiente array:
+// [
+//   {name: 'Mati', age: 25, height: 1.77},
+//   {name: 'Franco', age: 26, height: 1.85},
+//   {name: 'Toni', age: 30, height: 1.75},
+//   {name: 'Leo', age: 40, height: 1.83}
+// ]
+
+var specialSort = function(array, orderFunction) {
+  // Tu código aca:
+
+}
+
+// ----- Closures -----
+
+// EJERCICIO 10
+// Implementar la función closureDetect que recibe como parámetro:
+//  - Un array (symptoms) que va a contener en cada posición un string representando un
+//    síntoma médico de alguna enfermedad
+//  - Un número (min) que va a indicar la cantidad mínima de síntomas que debe tener un
+//    paciente para considerar que posee la enfermedad
+// Ejemplos:
+//   var symptoms = ['fever', 'dry cough', 'tiredness', 'sore throat', 'diarrhoea', 'loss of taste', 'loss of smell'];
+//   var covidDetector = closureDetect(symptoms, 3);
+//
+//   var personOne = {
+//     name: 'Franco',
+//     age: 26,
+//     symptoms: ['fever', 'congestion', 'loss of taste', 'tiredness']
+//   }
+//
+//   var personTwo = {
+//     name: 'Toni',
+//     age: 30,
+//     symptoms: ['congestion', 'tiredness']
+//   }
+//
+//   covidDetector(personOne); --> true
+//   covidDetector(personTwo); --> false
+//  [Observar los tests para otros casos]
+
+function closureDetect(symptoms, min) {
+  // Tu código aca:
+
+}
+
+// -------------------
+
+module.exports = {
+  isAncestor,
+  secuenciaHenry,
+  LinkedList,
+  Queue,
+  cardGame,
+  binarySearch,
+  specialSort,
+  closureDetect,
+  BinarySearchTree,
+  mergeLinkedLists
+}
