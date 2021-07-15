b4b3a83ab1fb90bf4b5726b1a03b13f8810b4d9d
Diego Rodriguez <dierodz91@gmail.com>
2021-07-15 17:49:48 -0300

    update ci

--- .github/action_activate.js
+++ .github/action_activate.js
@@ -1,13 +1,31 @@
 const fs = require("fs");
 const path = require("path");
 const simpleGit = require("simple-git");
-try {
-  const pathName = path.join(__dirname, "wf");
-  const pathNameRenamed = path.join(__dirname, "workflows");
-  const pathToAdd = path.join(pathNameRenamed, "node.js.yml");
-  if (fs.existsSync(pathName) && !fs.existsSync(pathNameRenamed)) {
-    fs.renameSync(pathName, pathNameRenamed);
-    const git = simpleGit();
-    git.add([pathToAdd]);
-  }
-} catch {}
+const gitRemoteOriginUrl = require("git-remote-origin-url");
+const GitUrlParse = require("git-url-parse");
+const chalk = require("chalk");
+gitRemoteOriginUrl()
+  .then((remote) => {
+    const { owner: github, name: repo } = GitUrlParse(remote);
+    if (github === "soyHenry") {
+      throw new Error(
+        `ERROR EN EL REPOSITORIO
+Debes clonar el mismo desde tu cuenta y no desde http://github.com/${github}/${repo}
+te recomendamos volver a clonar desde la cuenta correcta o contacta a tu instruvtor.`
+      );
+    }
+    try {
+      const pathName = path.join(__dirname, "wf");
+      const pathNameRenamed = path.join(__dirname, "workflows");
+      const pathToAdd = path.join(pathNameRenamed, "node.js.yml");
+      if (fs.existsSync(pathName) && !fs.existsSync(pathNameRenamed)) {
+        fs.renameSync(pathName, pathNameRenamed);
+        const git = simpleGit();
+        git.add([pathToAdd]);
+      }
+    } catch {}
+  })
+  .catch((err) => {
+    console.log(chalk.white.bgRed.bold(err.message));
+    process.exit(1);
+  });
--- .github/reporter.js
+++ .github/reporter.js
@@ -1,48 +1,37 @@
-const { createClient } = require("@supabase/supabase-js");
+const axios = require("axios").default;
 
 const gitRemoteOriginUrl = require("git-remote-origin-url");
 const GitUrlParse = require("git-url-parse");
 
 module.exports = function report(data) {
-  const supabaseUrl = "https://xezlkjddhfzkqchdwhxc.supabase.co";
-  const supabaseKey =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxNjQxNzM1MywiZXhwIjoxOTMxOTkzMzUzfQ.hMtXLyFhbhz7HiVvX_ogquA1kTKTdCABLs2EKZCsLX0";
-  const supabase = createClient(supabaseUrl, supabaseKey);
   try {
     const {
-      numTotalTests: totalTests,
-      numPendingTests: pendingTests,
-      numFailedTests: failedTests,
-      numPassedTests: passedTests,
-      testResults,
+      numTotalTestSuites,
+      numPassedTestSuites,
+      numFailedTestSuites,
+      numTotalTests,
     } = data;
-    if (totalTests > 0)
+    let { testResults } = data;
+    if (numTotalTests > 0)
       gitRemoteOriginUrl()
         .then((remote) => {
           const { name: repo, owner: github } = GitUrlParse(remote);
-          return supabase.from("evaluationTests").insert([
+          return axios.post(
+            "https://learning.soyhenry.com/toolbox/checkpoint-report/report/check",
             {
-              totalTests,
-              pendingTests,
-              failedTests,
-              passedTests,
-              remote: process.env.GITHUB_ACTIONS || false,
-              testResults: testResults.reduce((result, { testResults }) => {
-                const mapResult = testResults.map(({ status, title }) => ({
-                  status,
-                  title,
-                }));
-                return [...result, ...mapResult];
-              }, []),
+              numTotalTestSuites,
+              numPassedTestSuites,
+              numFailedTestSuites,
               repo,
               github,
-            },
-          ]);
+              githubsha: process.env.GITHUB_SHA,
+            }
+          );
         })
-        .then((results) => {})
         .catch((error) => console.error(error));
   } catch (error) {
     console.error(error);
   }
+  data.success = true;
   return data;
 };
